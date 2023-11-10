9b4ecca506d4fb3978206c3683035751b119fc96
pablokz1 <pablo.carvalho16@hotmail.com>
2023-11-09 22:54:07 -0300

    Modularazação do botão de voltar

--- src/js/myProfile.js
+++ src/js/myProfile.js
@@ -1,7 +1,3 @@
-function goBack() {
-    window.history.back();
-}
-
 function notice() {
     alert("Desculpe, mas esta funcionalidade ainda não está disponível. Estamos trabalhando nisso e ela será implementada em uma próxima atualização. Agradecemos pela sua compreensão e paciência!")
 }
