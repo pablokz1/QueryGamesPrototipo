3482ef350a9869a70958ee9c7fd7565f8f1b2738
pablokz1 <pablo.carvalho16@hotmail.com>
2023-11-12 11:23:24 -0300

    Implementado botão de mostrar senha

--- src/js/login.js
+++ src/js/login.js
@@ -13,3 +13,16 @@ function login() {
         window.location.href = '../index.html';
     }
 }
+
+function showPassword() {
+    var inputPass = document.getElementById('password');
+    var btnShowPass = document.getElementById('btn-password');
+
+    if(inputPass.type === 'password') {
+        inputPass.setAttribute('type', 'text');
+        btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill');
+    } else {
+        inputPass.setAttribute('type', 'password');
+        btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill');
+    }
+}
